110e82399bc5f81d5decdea9d320b6132bc7eabb
Suyeong RHIE <10545351+noxowl@users.noreply.github.com>
2024-07-29 01:28:05 +0900

    modify serial

---
+++ 0_final_exam_project/DistanceMeasurement/DistanceMeasurement.ino
@@ -0,0 +1,35 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.write(intToUnit8(voltageToDistance(analogueToVoltage(readAnalogue(0)))));
+  delay(50);
+}
+
+int readAnalogue(int pin) {
+  int scan = 0;
+  int scanRate = 30;
+  int scanTried = scanRate;
+  while (scanTried > 0) {
+    scan = scan + analogRead(pin);
+    scanTried--;
+  }
+  return scan / scanRate;
+}
+
+int analogueToVoltage(int source) {
+  // convert to 0 to 5V (but GP2Y0E03 using 3.3V)
+  // Serial.println(source);
+  return map(source, 0, 1023, 0, 500);
+} 
+
+int voltageToDistance(int source) {
+  // convert voltage to distance for sharp GP2Y0E03 by datasheet value (0.5V ~ 2.2V -> 50cm ~ 4cm)
+  // Serial.println(source);
+  return map(source, 4, 220, 50, 4);
+}
+
+uint8_t intToUnit8(int source) {
+  return source;
+}
